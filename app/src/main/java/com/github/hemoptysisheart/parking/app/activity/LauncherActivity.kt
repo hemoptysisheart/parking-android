9d0a04951a14371347885d30f8bf349a2275b49b
H2 <hemoptysisheart@gmail.com>
2022-10-26 00:41:54 +0900

    💄 스플래시용 런처 액티비티 추가.

---
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/activity/LauncherActivity.kt
@@ -0,0 +1,68 @@
+package com.github.hemoptysisheart.parking.app.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.github.hemoptysisheart.parking.R
+import com.github.hemoptysisheart.parking.ui.theme.ParkingTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+
+/**
+ * 앱 시작 액티비티.
+ */
+@AndroidEntryPoint
+class LauncherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParkingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    LauncherLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LauncherLayout() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.label_please_wait), fontSize = 44.sp, fontWeight = FontWeight.ExtraBold)
+    }
+    LaunchedEffect(true) {
+        delay(1_500L)
+        context.startActivity(Intent(context, MainActivity::class.java))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LauncherLayoutPreview() {
+    ParkingTheme {
+        LauncherLayout()
+    }
+}

6ae06ebb3615135c628d969829f1ac63f29d80db
H2 <hemoptysisheart@gmail.com>
2022-11-13 14:30:37 +0900

    💄 장소 검색어 입력창 추가.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/component/search/SearchHeader.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/component/search/SearchHeader.kt
@@ -1,32 +1,43 @@
 package com.github.hemoptysisheart.parking.app.ui.component.search
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.github.hemoptysisheart.parking.R
+import com.github.hemoptysisheart.parking.app.ui.configuration.LogicConstant.TAG_COMPOSE
+import com.github.hemoptysisheart.parking.app.viewmodel.SearchViewModel
+import com.github.hemoptysisheart.parking.core.dummy.model.DummyPlaceModel
 import com.github.hemoptysisheart.parking.ui.theme.ParkingTheme
 
 @Composable
-fun SearchHeader(navController: NavHostController = rememberNavController()) {
+fun SearchHeader(viewModel: SearchViewModel = hiltViewModel()) {
+    val navController: NavHostController = rememberNavController()
+    val query by viewModel.query.collectAsState()
+
     Row(
-        Modifier
+        modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp)
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             Icons.Default.ArrowBack,
@@ -37,6 +48,21 @@ fun SearchHeader(navController: NavHostController = rememberNavController()) {
                 .border(1.dp, Color.LightGray, RoundedCornerShape(20.dp))
                 .padding(5.dp)
         )
+        Spacer(Modifier.width(10.dp))
+        BasicTextField(
+            value = query,
+            onValueChange = {
+                Log.v(TAG_COMPOSE, "#search.onChange args : query=$it")
+                viewModel.search(it.trim())
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White, RoundedCornerShape(20.dp))
+                .border(1.dp, Color.LightGray, RoundedCornerShape(20.dp))
+                .padding(10.dp, 6.dp),
+            maxLines = 1
+        )
+        // TODO 검색 필터 추가.
     }
 }
 
@@ -44,6 +70,6 @@ fun SearchHeader(navController: NavHostController = rememberNavController()) {
 @Preview
 fun SearchHeaderPreview() {
     ParkingTheme {
-        SearchHeader()
+        SearchHeader(SearchViewModel(DummyPlaceModel))
     }
 }
--- app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/SearchViewModel.kt
@@ -1,9 +1,11 @@
 package com.github.hemoptysisheart.parking.app.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.hemoptysisheart.parking.core.model.PlaceModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
 
@@ -15,9 +17,19 @@ class SearchViewModel @Inject constructor(
         private val TAG = SearchViewModel::class.simpleName
     }
 
+    val query = MutableStateFlow("")
+
+    fun search(query: String) {
+        viewModelScope.launch {
+            this@SearchViewModel.query.emit(query)
+        }
+    }
+
+    @Deprecated("prototyping code")
     val places = MutableStateFlow(placeModel.places)
 
+    @Deprecated("prototyping code")
     fun read(id: UUID) = placeModel.read(id)
 
-    override fun toString() = "$TAG(places=$places)"
+    override fun toString() = "$TAG(query=${query.value}, places=$places)"
 }

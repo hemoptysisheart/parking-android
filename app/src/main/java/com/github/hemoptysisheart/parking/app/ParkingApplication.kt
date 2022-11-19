39011ee1c05a0a2883a0a121cf6db177c1e58051
H2 <hemoptysisheart@gmail.com>
2022-11-19 09:04:50 +0900

    ⚗️ 위치정보 갱신을 `Application`으로 이동하기.
    부드럽게 권한 부여 UI와 연결할 방법이 없기 때문에 실패한 실험.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ParkingApplication.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ParkingApplication.kt
@@ -2,8 +2,12 @@ package com.github.hemoptysisheart.parking.app
 
 import android.app.Application
 import android.content.SharedPreferences
+import android.os.Looper
 import android.util.Log
-import com.github.hemoptysisheart.parking.core.model.LocationTrackerModel
+import com.github.hemoptysisheart.parking.core.model.LocationModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -19,15 +23,25 @@ class ParkingApplication : Application() {
     lateinit var sharedPreferences: SharedPreferences
 
     @Inject
-    lateinit var locationTrackerModel: LocationTrackerModel
+    lateinit var locationClient: FusedLocationProviderClient
+
+    @Inject
+    lateinit var locationModel: LocationModel
 
     override fun onCreate() {
         super.onCreate()
 
         MainScope().launch {
             Log.i(TAG, "#onCreate start process initialize.")
-
-            locationTrackerModel.start()
+            configureSensor()
         }
+    }
+
+    private suspend fun configureSensor() {
+        // Location
+        locationClient.requestLocationUpdates(
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5_000L), {},
+            Looper.getMainLooper()
+        )
     }
 }

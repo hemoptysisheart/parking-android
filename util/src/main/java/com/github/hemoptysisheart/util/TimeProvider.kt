c65b04339285286b315161c307266dacc76147fc
H2 <hemoptysisheart@gmail.com>
2022-12-17 22:16:20 +0900

    🩹 시각정보 기준 메서드를 일부 변경.

--- util/src/main/java/com/github/hemoptysisheart/util/TimeProvider.kt
+++ util/src/main/java/com/github/hemoptysisheart/util/TimeProvider.kt
@@ -10,21 +10,48 @@ import java.time.*
 interface TimeProvider {
     val zoneId: ZoneId
 
+    /**
+     * @see Instant.now
+     */
     fun instant(): Instant
 
+    /**
+     * @see ZonedDateTime.now
+     */
     fun zonedDateTime() = ZonedDateTime.ofInstant(instant(), zoneId)
 
+    /**
+     * @see OffsetTime.now
+     */
     fun offsetTime() = OffsetTime.ofInstant(instant(), zoneId)
 
+    /**
+     * @see OffsetDateTime.now
+     */
     fun offsetDateTime() = OffsetDateTime.ofInstant(instant(), zoneId)
 
+    /**
+     * @see LocalDate.now
+     */
     fun localDate() = LocalDate.ofInstant(instant(), zoneId)
 
+    /**
+     * @see LocalTime.now
+     */
     fun localTime() = LocalTime.ofInstant(instant(), zoneId)
 
+    /**
+     * @see LocalDateTime.now
+     */
     fun localDateTime() = LocalDateTime.ofInstant(instant(), zoneId)
 
-    fun year() = Year.from(instant())
+    /**
+     * @see Year.now
+     */
+    fun year() = Year.from(localDate())
 
-    fun yearMonth() = YearMonth.from(instant())
+    /**
+     * @see YearMonth.now
+     */
+    fun yearMonth() = YearMonth.from(localDate())
 }

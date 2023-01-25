aaa460c224f7c350d07e8fbbe66b6bf3ef9f0b1c
H2 <hemoptysisheart@gmail.com>
2023-01-25 14:16:37 +0900

    🚚 UI 함수 패키지 변경.
    액티비티에 남은 함수를 이동.

---
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/screen/LauncherScreen.kt
@@ -0,0 +1,55 @@
+package com.github.hemoptysisheart.parking.app.ui.screen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.github.hemoptysisheart.parking.R
+import com.github.hemoptysisheart.parking.app.activity.MainActivity
+import com.github.hemoptysisheart.parking.ui.theme.ParkingTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun LauncherScreen() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.label_welcome_message, stringResource(R.string.app_name)),
+            modifier = Modifier.fillMaxWidth(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center
+        )
+    }
+    LaunchedEffect(true) {
+        delay(3000L)
+        context.startActivity(Intent(context, MainActivity::class.java))
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+@SuppressLint("ComposableNaming")
+fun preview_LauncherLayout() {
+    ParkingTheme {
+        LauncherScreen()
+    }
+}

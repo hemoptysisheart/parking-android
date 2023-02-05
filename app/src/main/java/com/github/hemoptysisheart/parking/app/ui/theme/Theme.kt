1bd159eae7471d9542d4ce462e20731e734c9248
H2 <hemoptysisheart@gmail.com>
2023-02-05 19:58:36 +0900

    💄 다크모드 비활성화.
    다크모드가 활성화되면서 텍스트 입력이 보이지 않아서 비활성화.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/theme/Theme.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/theme/Theme.kt
@@ -2,15 +2,8 @@ package com.github.hemoptysisheart.parking.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -26,6 +19,13 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
+
+private val DarkColorPalette = LightColorPalette
+//private val DarkColorPalette = darkColors(
+//    primary = Purple200,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//)
 
 @Composable
 fun ParkingTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {

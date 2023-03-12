15adb41fb78567b44269ff0592a70035457a5260
H2 <hemoptysisheart@gmail.com>
2023-03-12 14:18:01 +0900

    🐛 포어그라운드 카운터 로직 수정.
    액티비티 이동시 라이프사이클 이벤트 실행 순서로 인한 버그를 수정.
    
    `ParkingApplication.activityCallbacks#minBackgroundDuration` 주석을 참조.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ParkingApplication.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ParkingApplication.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.app.Application
 import android.os.Bundle
 import android.util.Log
+import com.github.hemoptysisheart.parking.app.activity.LauncherActivity
+import com.github.hemoptysisheart.parking.app.activity.MainActivity
 import com.github.hemoptysisheart.parking.core.logging.AndroidLoggingHandler
 import com.github.hemoptysisheart.parking.core.model.PreferencesModel.ExecutionPreferencesModel
 import com.github.hemoptysisheart.parking.domain.ExecutionPreferences
@@ -12,7 +14,8 @@ import com.github.hemoptysisheart.util.TimeProvider
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicInteger
+import java.time.Duration
+import java.time.Instant
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -22,7 +25,15 @@ class ParkingApplication : Application() {
     }
 
     private val activityCallbacks = object : ActivityLifecycleCallbacks {
-        private val counter = AtomicInteger()
+        private val countLock = Any()
+        private var count = 0
+        private var countDecreasedAt: Instant = Instant.MIN
+
+        /**
+         * 단순히 액티비티를 이동하더라도 [Activity.onPause]를 먼저 호출하기 때문에 [LauncherActivity]->[MainActivity]로 이동해도
+         * 포어그라운드 실행이 2번으로 계산된다. 그래서 [count]가 `0`인 상태를 100ms 이상인 경우만 포어그라운드로 돌아온 것으로 간주한다.
+         */
+        private val minBackgroundDuration = Duration.ofMillis(100L)
 
         override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
 
@@ -31,18 +42,23 @@ class ParkingApplication : Application() {
         override fun onActivityResumed(activity: Activity) {
             Log.v(TAG, "#activityCallbacks.onActivityResumed args : activity=$activity")
 
-            val count = counter.incrementAndGet()
-            Log.i(TAG, "#activityCallbacks.onActivityResumed : count=$count")
-            if (1 == count) {
-                (executionPreferences as ExecutionPreferencesModel).increaseForeground(timeProvider.instant())
+            synchronized(countLock) {
+                count++
+                Log.i(TAG, "#activityCallbacks.onActivityResumed : count=$count")
+                if (1 == count && minBackgroundDuration < Duration.between(countDecreasedAt, timeProvider.instant())) {
+                    (executionPreferences as ExecutionPreferencesModel).increaseForeground(timeProvider.instant())
+                }
             }
         }
 
         override fun onActivityPaused(activity: Activity) {
             Log.v(TAG, "#activityCallbacks.onActivityPaused args : activity=$activity")
 
-            val count = counter.decrementAndGet()
-            Log.i(TAG, "#activityCallbacks.onActivityPaused : count=$count")
+            synchronized(countLock) {
+                count--
+                countDecreasedAt = timeProvider.instant()
+                Log.i(TAG, "#activityCallbacks.onActivityPaused : count=$count")
+            }
         }
 
         override fun onActivityStopped(activity: Activity) {}

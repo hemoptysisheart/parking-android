877d1ced3801a6d750ddccb3c254ef3e79f8ed20
H2 <hemoptysisheart@gmail.com>
2023-03-18 11:00:03 +0900

    ✨ 지도에 경로 표시.
    경로 검색결과를 지도에 표시.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/component/Map.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/component/Map.kt
@@ -8,7 +8,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.hemoptysisheart.parking.R
@@ -20,6 +22,7 @@ import com.github.hemoptysisheart.parking.core.logging.logArgs
 import com.github.hemoptysisheart.parking.domain.Location
 import com.github.hemoptysisheart.parking.domain.RecommendItemLocation
 import com.github.hemoptysisheart.parking.domain.Route
+import com.github.hemoptysisheart.parking.ui.theme.LightSkyBlue
 import com.github.hemoptysisheart.parking.ui.theme.ParkingTheme
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
@@ -27,13 +30,19 @@ import com.google.maps.android.compose.*
 /**
  * 기본 지도 출력하기.
  *
+ * @param destination 목적지.
+ * @param parkingList 주차장 목록. TODO `routeList`로 대체.
+ * @param routeList 경로 목록.
+ * @param focusedRoute 포커스를 가진 목록.
  * @param cameraPositionState 지도 카메라 위치.
+ * @param onMapClick 맵 선택시 처리 로직.
  */
 @Composable
 fun Map(
     destination: Location? = null,
     parkingList: List<RecommendItemLocation> = listOf(),
     routeList: List<Route> = listOf(),
+    focusedRoute: Route? = null,
     cameraPositionState: CameraPositionState = rememberCameraPositionState(),
     onMapClick: (LatLng) -> Unit = { Log.v(TAG_COMPOSE, "#onMapClick called.") }
 ) {
@@ -42,6 +51,7 @@ fun Map(
         "Map",
         "destination" to destination,
         "parkingList" to parkingList,
+        "focusedRoute" to focusedRoute,
         "cameraPositionState" to cameraPositionState,
         "onMapClick" to onMapClick
     )
@@ -83,10 +93,15 @@ fun Map(
                 title = it.item.name,
                 icon = bitmapDescriptor(context, R.drawable.map_marker_parking)
             )
-
-            for (route in routeList) {
-                Polyline(points = route.driving.overview.points, color = Color.Blue)
-                Polyline(points = route.walking.overview.points, color = Color.Gray)
+        }
+        focusedRoute?.run {
+            Polyline(points = driving.overview.points, color = Blue, width = 15f, zIndex = 1f)
+            Polyline(points = walking.overview.points, color = Gray, width = 15f, zIndex = 1f)
+        }
+        for (route in routeList) {
+            if (focusedRoute != route) {
+                Polyline(points = route.driving.overview.points, color = LightSkyBlue)
+                Polyline(points = route.walking.overview.points, color = LightGray)
             }
         }
     }

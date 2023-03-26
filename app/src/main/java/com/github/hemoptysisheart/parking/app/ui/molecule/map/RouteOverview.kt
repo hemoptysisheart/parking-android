93bad66aa32cdece49838fee8d9763c785f5ebb1
H2 <hemoptysisheart@gmail.com>
2023-03-26 22:09:11 +0900

    💄 마지막 걷기 경로를 잘 보이게 표시.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/molecule/map/RouteOverview.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/molecule/map/RouteOverview.kt
@@ -3,6 +3,10 @@ package com.github.hemoptysisheart.parking.app.ui.molecule.map
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.github.hemoptysisheart.parking.app.domain.RouteImpl
+import com.github.hemoptysisheart.parking.app.ui.support.MAP_ROUTE_OVERVIEW_DRIVING_FOCUSED_Z_INDEX
+import com.github.hemoptysisheart.parking.app.ui.support.MAP_ROUTE_OVERVIEW_DRIVING_Z_INDEX
+import com.github.hemoptysisheart.parking.app.ui.support.MAP_ROUTE_OVERVIEW_WALKING_FOCUSED_Z_INDEX
+import com.github.hemoptysisheart.parking.app.ui.support.MAP_ROUTE_OVERVIEW_WALKING_Z_INDEX
 import com.github.hemoptysisheart.parking.core.extension.points
 import com.github.hemoptysisheart.parking.domain.Route
 import com.github.hemoptysisheart.parking.ui.theme.LightSkyBlue
@@ -12,11 +16,29 @@ import com.google.maps.android.compose.Polyline
 fun RouteOverview(route: Route, focused: Boolean) {
     if (route is RouteImpl && route.initialized) {
         if (focused) {
-            Polyline(points = route.driving.overview.points, color = Color.Blue, width = 15f, zIndex = 1f)
-            Polyline(points = route.walking.overview.points, color = Color.Gray, width = 15f, zIndex = 1f)
+            Polyline(
+                points = route.driving.overview.points,
+                color = Color.Blue,
+                width = 15f,
+                zIndex = MAP_ROUTE_OVERVIEW_DRIVING_FOCUSED_Z_INDEX
+            )
+            Polyline(
+                points = route.walking.overview.points,
+                color = Color.Gray,
+                width = 15f,
+                zIndex = MAP_ROUTE_OVERVIEW_WALKING_FOCUSED_Z_INDEX
+            )
         } else {
-            Polyline(points = route.driving.overview.points, color = LightSkyBlue)
-            Polyline(points = route.walking.overview.points, color = Color.LightGray)
+            Polyline(
+                points = route.driving.overview.points,
+                color = LightSkyBlue,
+                zIndex = MAP_ROUTE_OVERVIEW_DRIVING_Z_INDEX
+            )
+            Polyline(
+                points = route.walking.overview.points,
+                color = Color.LightGray,
+                zIndex = MAP_ROUTE_OVERVIEW_WALKING_Z_INDEX
+            )
         }
     }
 }

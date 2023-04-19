b85fd2585d63cdd490b957c260ed5347810ba263
H2 <hemoptysisheart@gmail.com>
2023-04-19 23:48:42 +0900

    ⬜️ TODO 추가.
    이름을 바꿔야 할 것 같은데 뭘로 바꾸지.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/navigation/AbstractPageNavigation.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/navigation/AbstractPageNavigation.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.navigation.NavController
 import com.github.hemoptysisheart.parking.core.util.Logger
 
+/**
+ * TODO [`*PageNavigation` 클래스 이름 변경해야 하나?](https://github.com/hemoptysisheart/parking-android/issues/79)
+ */
 abstract class AbstractPageNavigation(
     protected val navController: NavController
 ) {

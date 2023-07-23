0efffdfca4cc1c4a9d276ed4b4490118f2b5b411
H2 <hemoptysisheart@gmail.com>
2023-07-23 21:11:12 +0900

    🔧 색상 재정의 쉽도록 명시.
    [Figma Material Theme Builder](https://www.figma.com/community/plugin/1034969338659738588/Material-Theme-Builder) 기본값에 맞춰 지정.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/theme/Color.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/theme/Color.kt
@@ -2,12 +2,86 @@ package com.github.hemoptysisheart.parking.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Light Mode
+
+val LightPrimary = Color(0xFF6750A4)
+val LightPrimaryContainer = Color(0xFFEADDFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnPrimaryContainer = Color(0xFF21005D)
+val LightInversePrimary = Color(0xFFD0BCFF)
+
+val LightSecondary = Color(0xFF625B71)
+val LightOnSecondary = Color(0xFFFFFFFF)
+val LightSecondaryContainer = Color(0xFFE8DEF8)
+val LightOnSecondaryContainer = Color(0xFF1D192B)
+
+val LightTertiary = Color(0xFF7D5260)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightTertiaryContainer = Color(0xFFFFD8E4)
+val LightOnTertiaryContainer = Color(0xFF31111D)
+
+val LightBackground = Color(0xFFFFFBFE)
+val LightOnBackground = Color(0xFF1C1B1F)
+
+val LightSurface = Color(0xFFFEF7FF)
+val LightOnSurface = Color(0xFF1D1B20)
+val LightSurfaceVariant = Color(0xFFE6E0E9)
+val LightOnSurfaceVariant = Color(0xFF49454F)
+val LightSurfaceTint = LightPrimary
+val LightInverseSurface = Color(0xFF322F35)
+val LightInverseOnSurface = Color(0xFFF5EFF7)
+
+val LightError = Color(0xFFB3261E)
+val LightErrorContainer = Color(0xFFF9DEDC)
+val LightOnError = Color(0xFFFFFFFF)
+val LightOnErrorContainer = Color(0xFF410E0B)
+
+val LightOutline = Color(0xFF79747E)
+val LightOutlineVariant = Color(0xFFCAC4D0)
+val LightScrim = Color.Black
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Dark Mode
+
+val DarkPrimary = Color(0xFFD0BCFF)
+val DarkOnPrimary = Color(0xFF381E72)
+val DarkPrimaryContainer = Color(0xFF4F378B)
+val DarkOnPrimaryContainer = Color(0xFFEADDFF)
+val DarkInversePrimary = Color(0xFFD0BCFF)
+
+val DarkSecondary = Color(0xFFCCC2DC)
+val DarkOnSecondary = Color(0xFF332D41)
+val DarkSecondaryContainer = Color(0xFF4A4458)
+val DarkOnSecondaryContainer = Color(0xFFE8DEF8)
+
+val DarkTertiary = Color(0xFFEFB8C8)
+val DarkOnTertiary = Color(0xFF492532)
+val DarkTertiaryContainer = Color(0xFF633B48)
+val DarkOnTertiaryContainer = Color(0xFFFFD8E4)
+
+val DarkBackground = Color(0xFF1D1B20)
+val DarkOnBackground = Color(0xFFE6E0E9)
+
+val DarkSurface = Color(0xFF1D1B20)
+val DarkOnSurface = Color(0xFFE6E0E9)
+val DarkSurfaceVariant = Color(0xFF49454F)
+val DarkOnSurfaceVariant = Color(0xFFE7E0EC)
+val DarkSurfaceTint = DarkPrimary
+val DarkInverseSurface = Color(0xFF322F35)
+val DarkInverseOnSurface = Color(0xFFF5EFF7)
+
+val DarkError = Color(0xFFF2B8B5)
+val DarkErrorContainer = Color(0xFF8C1D18)
+val DarkOnError = Color(0xFFF2B8B5)
+val DarkOnErrorContainer = Color(0xFFF9DEDC)
+
+val DarkOutline = Color(0xFF938F99)
+val DarkOutlineVariant = Color(0xFFCAC4D0)
+val DarkScrim = Color.Black
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 val LightSkyBlue = Color(0xFF87CEFA)
-
 val TextDefault = Color.Unspecified
 val TextNull = Color.LightGray

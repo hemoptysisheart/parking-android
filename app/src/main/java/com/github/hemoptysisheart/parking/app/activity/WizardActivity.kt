94e9a935ba8aac4fc41f7b071d79ee14d510f5fa
H2 <hemoptysisheart@gmail.com>
2023-08-05 15:52:39 +0900

    🎨 액티비티 설정 방식 변경.
    함수로 공통화 했던 부분을 제거하고, 함수는 프리뷰 전용으로 변경.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/activity/WizardActivity.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/activity/WizardActivity.kt
@@ -3,26 +3,36 @@ package com.github.hemoptysisheart.parking.app.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.github.hemoptysisheart.parking.app.interaction.baseInteraction
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.navigation.compose.rememberNavController
+import com.github.hemoptysisheart.parking.app.interaction.BaseInteraction
 import com.github.hemoptysisheart.parking.app.interaction.wizard.WizardNavGraph
 import com.github.hemoptysisheart.parking.app.ui.theme.ParkingTheme
 import com.github.hemoptysisheart.parking.core.util.AndroidLogger
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * 마법사 액티비티
+ *
+ * 앱 기본 설정과 사용 안내를 제공한다.
+ */
 @AndroidEntryPoint
 class WizardActivity : ComponentActivity() {
     companion object {
-        private const val TAG = "WizardActivity"
-        private val LOGGER = AndroidLogger(TAG)
+        private val LOGGER = AndroidLogger(WizardActivity::class)
     }
 
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         LOGGER.i("#onCreate args : savedInstanceState=$savedInstanceState")
         super.onCreate(savedInstanceState)
 
         setContent {
             ParkingTheme {
-                WizardNavGraph(baseInteraction(this))
+                WizardNavGraph(
+                    BaseInteraction(this, rememberNavController(), LocalSoftwareKeyboardController.current)
+                )
             }
         }
     }

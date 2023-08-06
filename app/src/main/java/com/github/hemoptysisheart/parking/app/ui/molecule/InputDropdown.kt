309e3a4c395d69d316fd6b30aa01c97c283c115d
H2 <hemoptysisheart@gmail.com>
2023-08-06 17:08:49 +0900

    💄 드랍다운 수정.
    텍스트필드 대신 버튼 사용.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/molecule/InputDropdown.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/molecule/InputDropdown.kt
@@ -1,17 +1,18 @@
 package com.github.hemoptysisheart.parking.app.ui.molecule
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,70 +22,56 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.github.hemoptysisheart.parking.app.ui.preview.LOGGER_PREVIEW
 import com.github.hemoptysisheart.parking.app.ui.preview.PreviewComponent
 import java.time.temporal.ChronoUnit
 import java.time.temporal.TemporalUnit
 
 @Composable
 fun <E> InputDropdown(
-        label: String,
+        label: String?,
         selected: E?,
-        values: List<Pair<out E?, String>>,
+        values: List<Pair<E?, String>>,
         expanded: Boolean,
         onToggleExpendRequest: (Boolean) -> Unit,
-        onValueChanged: (E?) -> Unit,
+        onValueSelected: (E?) -> Unit,
         modifier: Modifier = Modifier,
-        enabled: Boolean = true,
-        selectedUi: @Composable ((E?) -> Unit)? = null,
-        itemUi: @Composable ((String) -> Unit)? = null
+        enabled: Boolean = true
 ) {
     Row(
-            modifier = modifier
-                    .clickable {
-                        if (enabled) {
-                            onToggleExpendRequest(!expanded)
-                        }
-                    },
+            modifier = modifier,
             verticalAlignment = Alignment.CenterVertically
     ) {
-        TextLabelMedium(text = label, modifier = Modifier.weight(1F))
-        Spacer(modifier = Modifier.width(10.dp))
-        OutlinedTextField(
-                value = values.first { it.first == selected }.second,
-                onValueChange = { },
-                enabled = true,
-                readOnly = true,
-                placeholder = if (selectedUi == null) {
-                    { TextBodyMedium(text = values.first { it.first == selected }.second) }
+        label?.run {
+            TextLabelMedium(text = this, modifier = Modifier.weight(1F))
+            Spacer(modifier = Modifier.width(10.dp))
+        }
+        Column(horizontalAlignment = Alignment.End) {
+            OutlinedButton(
+                    onClick = { onToggleExpendRequest(!expanded) },
+                    enabled = enabled
+            ) {
+                TextBodyMedium(text = values.first { it.first == selected }.second)
+
+                if (expanded) {
+                    Icon(imageVector = Icons.Default.ArrowDropUp, contentDescription = null)
                 } else {
-                    { selectedUi(selected) }
-                },
-                trailingIcon = {
-                    if (expanded) {
-                        DropUpButton()
-                    } else {
-                        DropDownButton()
-                    }
-                },
-                colors = OutlinedTextFieldDefaults.colors(
-                        unfocusedPlaceholderColor = MaterialTheme.colorScheme.onSurface,
-                        focusedPlaceholderColor = MaterialTheme.colorScheme.onSurface,
-                        disabledPlaceholderColor = MaterialTheme.colorScheme.onSurface
-                )
-        )
-        DropdownMenu(expanded = expanded, onDismissRequest = { onToggleExpendRequest(false) }) {
-            values.forEach {
-                DropdownMenuItem(
-                        text = {
-                            if (itemUi == null) {
-                                TextLabelMedium(it.second)
-                            } else {
-                                itemUi(it.second)
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                }
+            }
+
+            DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { onToggleExpendRequest(false) }
+            ) {
+                values.forEach {
+                    DropdownMenuItem(
+                            text = { TextLabelMedium(it.second) },
+                            onClick = {
+                                onValueSelected(it.first)
+                                onToggleExpendRequest(false)
                             }
-                        },
-                        onClick = { onValueChanged(it.first) }
-                )
+                    )
+                }
             }
         }
     }
@@ -93,9 +80,6 @@ fun <E> InputDropdown(
 @Composable
 @Preview(showBackground = true)
 fun Preview_InputDropdown() {
-    var expanded by remember {
-        mutableStateOf(false)
-    }
     val values = listOf<Pair<TemporalUnit?, String>>(
             null to "(미설정)",
             ChronoUnit.CENTURIES to "세기",
@@ -106,42 +90,31 @@ fun Preview_InputDropdown() {
             ChronoUnit.MINUTES to "분",
             ChronoUnit.SECONDS to "초"
     )
-    var selected by remember {
-        mutableStateOf(values.random().first)
-    }
 
     PreviewComponent {
         Column(Modifier.padding(10.dp)) {
+            var e1 by remember("1") { mutableStateOf(false) }
+            var s1 by remember("1") { mutableStateOf<TemporalUnit?>(null) }
             InputDropdown(
                     label = "dropdown 입력",
-                    selected = null,
+                    selected = s1,
                     values = values,
-                    expanded = expanded,
-                    onToggleExpendRequest = {
-                        LOGGER_PREVIEW.d("#onToggleExpendRequest args : expend=$it")
-                        expanded = it
-                    },
-                    onValueChanged = {
-                        LOGGER_PREVIEW.d("#onValueChanged args : value=$it")
-                        selected = it
-                    }
+                    expanded = e1,
+                    onToggleExpendRequest = { e1 = it },
+                    onValueSelected = { s1 = it }
             )
 
             Divider(modifier = Modifier.padding(10.dp, 15.dp), thickness = 1.dp)
 
+            var e2 by remember("2") { mutableStateOf(false) }
+            var s2: TemporalUnit? by remember("2") { mutableStateOf(values.map { it.first }.filterNotNull().random()) }
             InputDropdown(
                     label = "dropdown 입력",
-                    selected = selected,
+                    selected = s2,
                     values = values,
-                    expanded = expanded,
-                    onToggleExpendRequest = {
-                        LOGGER_PREVIEW.v("#onToggleExpendRequest args : expend=$it")
-                        expanded = it
-                    },
-                    onValueChanged = {
-                        LOGGER_PREVIEW.d("#onValueChanged args : value=$it")
-                        selected = it
-                    }
+                    expanded = e2,
+                    onToggleExpendRequest = { e2 = it },
+                    onValueSelected = { s2 = it }
             )
         }
     }

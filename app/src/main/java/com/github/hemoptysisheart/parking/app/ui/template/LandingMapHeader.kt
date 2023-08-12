503ea2be7770559dacc133c76b56b97689731e8b
H2 <hemoptysisheart@gmail.com>
2023-08-13 08:47:12 +0900

    🐛 헤더 버튼을 텍스트필드에서 버튼으로 변경.
    색깔 때문에 텍스트필드를 썼는데, read-only 설정이라 클릭이 안된다.
    버튼 색깔을 바꿔서 수정.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/template/LandingMapHeader.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/template/LandingMapHeader.kt
@@ -1,20 +1,20 @@
 package com.github.hemoptysisheart.parking.app.ui.template
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.github.hemoptysisheart.parking.app.ui.molecule.SearchButton
 import com.github.hemoptysisheart.parking.app.ui.molecule.SettingsButton
@@ -36,23 +36,21 @@ fun LandingMapHeader(
                     .fillMaxWidth()
                     .background(Color.Transparent)
                     .padding(10.dp, 10.dp),
+            horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically
     ) {
-        OutlinedTextField(
-                value = "",
-                onValueChange = { },
+        OutlinedButton(
+                onClick = gotoDestinationSearch,
                 modifier = Modifier
-                        .weight(1F)
-                        .clickable { gotoDestinationSearch() },
-                readOnly = true,
-                leadingIcon = { SearchButton() },
-                placeholder = { TextBodyMedium(text = text) },
-                shape = RoundedCornerShape(30.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                        focusedContainerColor = MaterialTheme.colorScheme.background,
-                        unfocusedContainerColor = MaterialTheme.colorScheme.background
+                        .weight(1F),
+                colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                        contentColor = MaterialTheme.colorScheme.outlineVariant
                 )
-        )
+        ) {
+            SearchButton()
+            TextBodyMedium(text = text, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Start)
+        }
         Spacer(modifier = Modifier.width(10.dp))
         SettingsButton(onClick = gotoSetting, color = MaterialTheme.colorScheme.onBackground)
     }

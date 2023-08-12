b39fdc0b291c5c9cfcedfffeee1c9dbf1e549964
H2 <hemoptysisheart@gmail.com>
2023-08-12 20:25:22 +0900

    🔥 테스트용 샘플코드 삭제.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/ui/page/main/LandingMapPage.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/ui/page/main/LandingMapPage.kt
@@ -1,20 +1,7 @@
 package com.github.hemoptysisheart.parking.app.ui.page.main
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.hemoptysisheart.parking.app.interaction.main.LandingMapInteraction
-import com.github.hemoptysisheart.parking.app.ui.molecule.EasyButton
 import com.github.hemoptysisheart.parking.app.ui.page.LOGGER
 import com.github.hemoptysisheart.parking.app.ui.preview.PagePreview
 import com.github.hemoptysisheart.parking.app.ui.preview.PagePreviewContainer
@@ -30,42 +17,6 @@ fun LandingMapPage(
         viewModel: LandingMapViewModel = hiltBaseViewModel()
 ) {
     LOGGER.v("#LandingMapPage args : interaction=$interaction, viewModel=$viewModel")
-
-    val count by viewModel.count.collectAsStateWithLifecycle()
-
-    Column(
-            modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-            verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-                text = """
-                지도, 현재 위치, 오버레이 표시.
-                
-                목적지를 선택했을 경우와 비슷한 오버레이를 활용해서 사용자에게 정보를 전달하고 다음 행동을 유도한다.
-            """.trimIndent(),
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
-        )
-
-        EasyButton(onClick = interaction::gotoDestinationSearch, label = "목적지 검색")
-        EasyButton(onClick = interaction::gotoSetting, label = "설정")
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        Text(
-                text = "onResume : $count 회", modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-        )
-        EasyButton(onClick = viewModel::onProgress, label = "작업 시작")
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        EasyButton(onClick = viewModel::onError, label = "에러 발생")
-    }
 }
 
 @Composable
--- app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/LandingMapViewModel.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/LandingMapViewModel.kt
@@ -1,14 +1,6 @@
 package com.github.hemoptysisheart.parking.app.viewmodel
 
-import androidx.lifecycle.LifecycleOwner
-import com.github.hemoptysisheart.parking.R
-import com.github.hemoptysisheart.parking.core.util.AndroidMessageException
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import java.time.ZonedDateTime
-import java.util.concurrent.ThreadLocalRandom
-import java.util.concurrent.atomic.AtomicInteger
 import javax.inject.Inject
 
 /**
@@ -16,43 +8,7 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class LandingMapViewModel @Inject constructor() : BaseViewModel() {
-    private val showCounter = AtomicInteger()
-    val count = MutableStateFlow(0)
-
     init {
         logger.i("#init called.")
-    }
-
-    fun onProgress() {
-        logger.d("#onProgress called.")
-
-        launch(true) {
-            delay(ThreadLocalRandom.current().nextLong(1_000, 10_000))
-        }
-    }
-
-    override fun onResume(owner: LifecycleOwner) {
-        logger.d("#onResume args : owner=$owner")
-        super.onResume(owner)
-
-        launch {
-            count.emit(showCounter.incrementAndGet())
-        }
-    }
-
-    fun onError() {
-        logger.d("#onError called.")
-
-        launch {
-            delay(1500L)
-            when (ThreadLocalRandom.current().nextInt(2)) {
-                0 -> throw AndroidMessageException(null, R.string.global_header_error_exp_case_simple_detail)
-                1 -> throw AndroidMessageException(
-                        R.string.global_header_error_exp_case_full_title,
-                        R.string.global_header_error_exp_case_full_detail_template,
-                        ZonedDateTime.now()
-                )
-            }
-        }
     }
 }

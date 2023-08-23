a349bce455886deb43c9de3ed3960c1120e6cd06
H2 <hemoptysisheart@gmail.com>
2023-08-23 19:11:35 +0900

    ✨ 코루틴 완료 시그널 추가.

--- app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/github/hemoptysisheart/parking/app/viewmodel/BaseViewModel.kt
@@ -11,6 +11,8 @@ import com.github.hemoptysisheart.util.TimeProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.UUID
 import java.util.concurrent.atomic.AtomicInteger
@@ -29,17 +31,27 @@ open class BaseViewModel : ViewModel(), DefaultLifecycleObserver {
             val base: BaseViewModel,
             val key: UUID = UUID.randomUUID()!!
     ) {
+        /**
+         * [BaseViewModel.launchSignal]를 대행한다.
+         */
+        protected val launchSignal = base.launchSignal
+
+        /**
+         * [BaseViewModel.launch]를 대행한다.
+         */
         protected fun launch(
                 progress: Boolean = false,
+                requestSignal: Boolean = false,
                 context: CoroutineContext = EmptyCoroutineContext,
                 start: CoroutineStart = CoroutineStart.DEFAULT,
                 block: suspend CoroutineScope.() -> Unit
-        ): Job = base.launch(progress, context, start, block)
+        ): Job = base.launch(progress, requestSignal, context, start, block)
 
         override fun toString() = "base=$base, key=$key"
     }
 
     private val progressCounter = AtomicInteger()
+
     protected val logger = AndroidLogger(this::class)
 
     @Inject
@@ -48,8 +60,18 @@ open class BaseViewModel : ViewModel(), DefaultLifecycleObserver {
     @Inject
     lateinit var globalChannel: GlobalChannel
 
+    private val _launchSignal = MutableStateFlow(UUID.randomUUID())
+    protected val launchSignal: StateFlow<UUID> = _launchSignal
+
+    /**
+     * [viewModelScope]로 코루틴을 실행한다.
+     *
+     * @param progress 코루틴 실행중 프로그레스 인디케이터 표시 여부.
+     * @param requestSignal 코루틴 실행이 끝나면 [launchSignal] 갱신으로 신호를 보내도록 요청한다.
+     */
     protected fun launch(
             progress: Boolean = false,
+            requestSignal: Boolean = false,
             context: CoroutineContext = EmptyCoroutineContext,
             start: CoroutineStart = CoroutineStart.DEFAULT,
             block: suspend CoroutineScope.() -> Unit
@@ -60,7 +82,7 @@ open class BaseViewModel : ViewModel(), DefaultLifecycleObserver {
         return viewModelScope.launch(context, start) {
             try {
                 if (progress) {
-                    logger.v("#launch increase progress : key=$launchKey, now=${timeProvider.instant()}")
+                    logger.v("#launch increase progress : key=$launchKey")
                     progressCounter.incrementAndGet()
                     globalChannel.reportProgress(1)
                 }
@@ -74,9 +96,12 @@ open class BaseViewModel : ViewModel(), DefaultLifecycleObserver {
                 // TODO report error.
             } finally {
                 if (progress) {
-                    logger.v("#launch decrease progress : key=$launchKey, now=${timeProvider.instant()}")
+                    logger.v("#launch decrease progress : key=$launchKey")
                     progressCounter.decrementAndGet()
                     globalChannel.reportProgress(-1)
+                }
+                if (requestSignal) {
+                    _launchSignal.emit(UUID.randomUUID())
                 }
             }
         }

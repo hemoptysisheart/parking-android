d26ce3d1b9da223f6d7d13ddf597dfb33e3840cc
H2 <hemoptysisheart@gmail.com>
2023-08-28 23:52:15 +0900

    🩹 타입 변환 함수이름 변경.

--- core/src/main/java/com/github/hemoptysisheart/parking/core/domain/place/Place.kt
+++ core/src/main/java/com/github/hemoptysisheart/parking/core/domain/place/Place.kt
@@ -1,11 +1,12 @@
 package com.github.hemoptysisheart.parking.core.domain.place
 
 import com.github.hemoptysisheart.parking.domain.place.Geolocation
+import com.github.hemoptysisheart.parking.domain.place.Place
 import com.github.hemoptysisheart.parking.domain.place.PlaceType
 
-fun com.github.hemoptysisheart.parking.domain.place.Place.toLatLng() = geolocation.toLatLng()
+fun Place.toLatLng() = geolocation.toLatLng()
 
-fun com.github.hemoptysisheart.parking.client.google.data.Place.toDomain(type: PlaceType): PlaceGooglePlace {
+fun com.github.hemoptysisheart.parking.client.google.data.Place.toPlace(type: PlaceType): PlaceGooglePlace {
     val source = listOfNotNull(
             name,
             formattedAddress,

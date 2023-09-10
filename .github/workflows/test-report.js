50cb3b29a8125e52f7fb2d810312e97fe739865f
H2 <hemoptysisheart@gmail.com>
2023-09-10 12:37:21 +0900

    💄 보고서 레이아웃 개선.

--- .github/workflows/test-report.js
+++ .github/workflows/test-report.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
     var domain = window.location.origin;
+
     var reportFrame = $('#frame-report')[0];
+    reportFrame.loadReport = function(srcBt){
+        console.log('#loadReport', srcBt);
+        $(this).attr('src', domain + '/' + srcBt.attr('value'));
+    };
     console.log(origin, reportFrame);
 
-    $('button.bt-show-report').on('click', function() {
-        var bt = $(this);
-        var path = bt.attr('value');
-        var url = domain + '/' +path;
-        reportFrame.attr('src', url);
+    var buttonList = $('button.bt-show-report')
+    console.log(buttonList);
+    buttonList.on('click', function() {
+        reportFrame.loadReport($(this));
     });
+    reportFrame.loadReport($(buttonList[0]));
 });
